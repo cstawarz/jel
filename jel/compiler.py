3055403ac4a40cb83340af88413ac2b2d98a7ab1
Christopher Stawarz <cstawarz@mit.edu>
2013-10-18 14:45:29 -0400

    Include location info in compiler output

--- jel/compiler.py
+++ jel/compiler.py
@@ -51,8 +51,8 @@ class Compiler(object):
             setattr(self, gen_name, self._make_op_gen(code))
 
     def _make_op_gen(self, code):
-        def genop(*args):
-            self._ops[-1].append((code, args))
+        def genop(node, *args):
+            self._ops[-1].append((code, node.lineno, node.lexpos, args))
         return genop
 
     def _genops(self, node):
@@ -60,66 +60,66 @@ class Compiler(object):
 
     def or_expr(self, node):
         operand_ops = tuple(self.compile(o) for o in node.operands)
-        self.logical_or(*operand_ops)
+        self.logical_or(node, *operand_ops)
 
     def and_expr(self, node):
         operand_ops = tuple(self.compile(o) for o in node.operands)
-        self.logical_and(*operand_ops)
+        self.logical_and(node, *operand_ops)
 
     def binary_op_expr(self, node):
         self._genops(node.operands[0])
         self._genops(node.operands[1])
-        self.binary_op(self.binary_op_codes[node.op])
+        self.binary_op(node, self.binary_op_codes[node.op])
 
     def unary_op_expr(self, node):
         self._genops(node.operand)
-        self.unary_op(self.unary_op_codes[node.op])
+        self.unary_op(node, self.unary_op_codes[node.op])
 
     def comparison_expr(self, node):
         ops = tuple(self.comparison_op_codes[o] for o in node.ops)
         operand_ops = tuple(self.compile(o) for o in node.operands)
-        self.compare_op(ops, operand_ops)
+        self.compare_op(node, ops, operand_ops)
 
     def call_expr(self, node):
         self._genops(node.target)
         arg_ops = tuple(self.compile(arg) for arg in node.args)
-        self.call_function(*arg_ops)
+        self.call_function(node, *arg_ops)
 
     def subscript_expr(self, node):
         self._genops(node.target)
         self._genops(node.value)
-        self.binary_subscr()
+        self.binary_subscr(node)
 
     def attribute_expr(self, node):
         self._genops(node.target)
-        self.load_attr(node.name)
+        self.load_attr(node, node.name)
 
     def object_literal_expr(self, node):
         for key, value in node.items.items():
             self.load_const(key)
             self._genops(value)
-        self.build_object(len(node.items))
+        self.build_object(node, len(node.items))
 
     def array_literal_expr(self, node):
         for item in node.items:
             self._genops(item)
-        self.build_array(len(node.items))
+        self.build_array(node, len(node.items))
 
     def string_literal_expr(self, node):
-        self.load_const(node.value)
+        self.load_const(node, node.value)
 
     def number_literal_expr(self, node):
         assert node.tag is None
-        self.load_const(float(node.value))
+        self.load_const(node, float(node.value))
 
     def boolean_literal_expr(self, node):
-        self.load_const(node.value)
+        self.load_const(node, node.value)
 
     def null_literal_expr(self, node):
-        self.load_const(None)
+        self.load_const(node, None)
 
     def identifier_expr(self, node):
-        self.load_name(node.value)
+        self.load_name(node, node.value)
 
     def compile(self, root):
         self._ops.append([])
@@ -128,15 +128,21 @@ class Compiler(object):
 
     @classmethod
     def print_ops(cls, ops, indent=0):
-        for index, (op, args) in enumerate(ops):
+        for index, (op, lineno, lexpos, args) in enumerate(ops):
             op = cls.op_names[op]
-            print('{}{:4} {:15}'.format(' ' * indent, index, op), end='')
+            if isinstance(lineno, tuple):
+                loc = ','.join('{}:{}'.format(ln, lp)
+                               for ln, lp in zip(lineno, lexpos))
+            else:
+                loc = '{}:{}'.format(lineno, lexpos)
+            print('{}{:4} {:14} {}  '.format(' ' * indent, index, op, loc),
+                  end = '')
             if op == 'BINARY_OP':
                 print(cls.binary_op_names[args[0]])
             elif op == 'UNARY_OP':
                 print(cls.unary_op_names[args[0]])
             elif op == 'COMPARE_OP':
-                print(', '.join(cls.comparison_op_names[code]
+                print(','.join(cls.comparison_op_names[code]
                                 for code in args[0]))
                 cls._print_arg_ops(args[1], indent)
             elif op in ('CALL_FUNCTION', 'LOGICAL_AND', 'LOGICAL_OR'):
